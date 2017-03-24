796c623e0ea983d835e6f2b7dbb82595a26bb1e7
DESKTOP-6J0ROFG\TONTRUONG <tonvantruong@codelovers.vn>
2017-03-24 18:27:08 +0700

    Khởi tạo project

---
+++ PRJ_DEMO_GIT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PRJ_DEMO_GIT.MvcApplication" Language="C#" %>
---
+++ PRJ_DEMO_GIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PRJ_DEMO_GIT.Startup))]
+namespace PRJ_DEMO_GIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
